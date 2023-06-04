f151007eccdd11ec62732d1879db17fc0132dc27
Manish kumar <75328802+manishkumar123456@users.noreply.github.com>
2023-06-04 16:51:22 +0530

    Add files via upload

---
+++ day one .cpp
@@ -0,0 +1,25 @@
+//Set Matrix Zeros
+#include <bits/stdc++.h>
+
+void  setZeros(vector<vector<int>> & matrix )
+{
+  map<int,int> row;
+  map<int,int> col;
+
+  for (int i = 0; i <matrix.size(); i++) {
+		for (int j = 0; j <matrix[0].size(); j++) {
+			if (matrix[i][j] == 0) {
+				row[i] = 1;
+				col[j] = 1;
+			}
+		}
+    }
+	for(int i=0;i<matrix.size();i++){
+		for(int j=0;j<matrix[0].size();j++){
+			if(row[i] || col[j]){
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	//return matrix;
+}
